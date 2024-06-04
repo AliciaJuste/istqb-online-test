a759f9d3a7bf982cd0a6e63f5bc61d94dcfbaf10
Alice <aliciajuste@gmail.com>
2024-06-04 22:05:38 +0200

    Add Correct Answer button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.write("Hello World");
+let correctAnswersList = ["b", "a", "a", "b", "b", "a", "b", "d", "d", "c"];
 
 //Function for print some message in a container (div, p)
 function printMessage(message, idContainer) {
@@ -6,24 +6,40 @@ function printMessage(message, idContainer) {
     container.textContent = message;
 }
 
-//Function for cleaning all the answers of a question
-function clearAnswers(formId) {
-  let form = document.getElementById(formId);
-  form.reset();
-
-  let radioButtons = form.querySelectorAll('input[type=radio]');
-  radioButtons.forEach(function(radio) {
-    radio.checked = false;
-  });
-}
-
+//Displays a welcome message with the user's name
 function welcomeMessage(containerId, nameId) {
     let nameUser = document.getElementById(nameId).value;
     let message = "Hi " + nameUser + ", welcome to the ISTQB online test, try your best!"
+
     printMessage(message, containerId);
 }
 
-function seeAnswer(containerId, correctAnswer) {
+//Displays the correct answer for the question selected
+function seeAnswer(questionNumber) {
+  let correctAnswer = correctAnswersList[questionNumber-1]
   let message = "The correct answer is option " + correctAnswer;
-  printMessage(message, containerId)
+
+  printMessage(message, "responseAnswer" + questionNumber);
 }
+
+//Displays if the answer was correct or not
+function correctAnswer(questionNumber) {
+  let selectedAnswerElement = document.querySelector(`input[name="question${questionNumber}"]:checked`);
+  let correctAnswer = correctAnswersList[questionNumber-1];
+  let message = "";
+
+  if (selectedAnswerElement) {
+    let selectedAnswer = selectedAnswerElement.value;
+    
+    if (selectedAnswer === correctAnswer) {
+      message = "The answer is correct!";
+    } else {
+      message= "The answer is not correct."
+    }
+  } else {
+    message = "No answer selected";
+  }
+
+  printMessage(message, "responseAnswer" + questionNumber);
+}
+
